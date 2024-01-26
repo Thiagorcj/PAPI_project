e32e6bf19329359237764493b8b0a2f2e75efdda
Thiago Rodrigues <thiago.rodrigues@academico.ufpb.br>
2024-01-25 23:18:31 -0300

    Update_5

--- chatbot.py
+++ chatbot.py
@@ -3,31 +3,19 @@ import requests
 import os
 from Conection import translate_audio, transcription, resumo, stem_separation
 
+class UserState:
+    def __init__(self):
+        self.flag = '0'
+
+# Um dicionário para armazenar o estado de cada usuário
+user_states = {}
 
 diretorio_atual = os.path.dirname(os.path.abspath(__file__))
 chave = "6380261490:AAHduix4ROprVAgBnKU64EzuFUpJkm3WCXI"
 bot  = telebot.TeleBot(chave)#Cria o bot 
 download_url = f'https://api.telegram.org/file/bot{chave}/'
 flag = '0'
-#Função que processa o aúdio, que pode ser tanto no formato .ogg (voice) quanto mp3/mp4 (audio)
-def process_audio(mensagem):
-    #Audio file
-    if mensagem.content_type == 'audio':
-        message = mensagem.audio
-    elif mensagem.content_type == 'voice':
-        message = mensagem.voice
-    #Audio download link
-    audio_path = bot.get_file(message.file_id).file_path
-    audio_download_link = download_url+audio_path
-    #Download do audio
-    audio_file = requests.get(audio_download_link)
-    # Constrói o caminho para o arquivo de áudio
-    audio_file_name = os.path.join(diretorio_atual, f'{mensagem.from_user.first_name}_audio_{mensagem.id}.ogg')
-    open(audio_file_name, 'wb').write(audio_file.content)
-    bot.reply_to(mensagem,"Áudio processado com sucesso, aguarde um instante")
-    return audio_file_name
-
-# Algumas que achei interessante pra deixar o PAPI mais completo
+# Algumas coisas que achei interessante pra deixar o PAPI mais completo
 def randon_messages(mensagem):
     if mensagem.text == 'Bom dia' or mensagem.text == 'bom dia':
          bot.send_message(mensagem.from_user.id,f"Bom dia, {mensagem.from_user.first_name}, se quiser ajuda do PAPI, manda /help")
@@ -39,9 +27,9 @@ def randon_messages(mensagem):
          bot.send_message(mensagem.from_user.id,f"Olá, {mensagem.from_user.first_name}, se quiser ajuda do PAPI, manda /help")
 @bot.message_handler(func=randon_messages)
 
-#/start
 @bot.message_handler(commands=["start"])
 def responder(mensagem):
+    user_id = mensagem.from_user.id
     bot.reply_to(mensagem,'''Olá, eu sou o PAPI
 Você pode acessar os seguintes comandos:
 /resumo: Faço o resumo de um áudio.
@@ -62,79 +50,108 @@ Você pode acessar os seguintes comandos:
 /separate: Separo um elemento do audio.'''
     bot.reply_to(mensagem,texto)
 
+#Função que processa o aúdio, que pode ser tanto no formato .ogg (voice) quanto mp3/mp4 (audio)
+def process_audio(mensagem):
+    #Audio file
+    if mensagem.content_type == 'audio':
+        message = mensagem.audio
+    elif mensagem.content_type == 'voice':
+        message = mensagem.voice
+    #Audio download link
+    audio_path = bot.get_file(message.file_id).file_path
+    audio_download_link = download_url+audio_path
+    #Download do audio
+    audio_file = requests.get(audio_download_link)
+    # Constrói o caminho para o arquivo de áudio
+    audio_file_name = os.path.join(diretorio_atual, f'{mensagem.from_user.first_name}_audio_{mensagem.id}.ogg')
+    open(audio_file_name, 'wb').write(audio_file.content)
+    bot.reply_to(mensagem,"Áudio processado com sucesso, aguarde um instante")
+    return audio_file_name
 # /resumo
 @bot.message_handler(commands=['resumo'])
 def resumir(mensagem):
-    global flag 
-    flag = '1'
-    bot.send_message(mensagem.from_user.id,f"{mensagem.from_user.first_name}, manda o audio pro PAPI aqui, que eu faço um resumão pra você")
+    user_id = mensagem.from_user.id
+    user_states[user_id] = UserState()
+    user_states[user_id].flag = '1'
+    bot.send_message(user_id, f"{mensagem.from_user.first_name}, manda o audio pro PAPI aqui, que eu faço um resumão pra você")
 
 # /totext
 @bot.message_handler(commands=['totext'])
 def totext(mensagem):
-    global flag
-    flag = '2'
-    bot.send_message(mensagem.from_user.id,f"{mensagem.from_user.first_name}, manda a audio pro PAPI aqui, que eu transcrevo para você")
+    user_id = mensagem.from_user.id
+    user_states[user_id] = UserState()
+    user_states[user_id].flag = '2'
+    bot.send_message(user_id, f"{mensagem.from_user.first_name}, manda a audio pro PAPI aqui, que eu transcrevo para você")
 
-#/translate
+# /translate
 @bot.message_handler(commands=['translate'])
 def translate(mensagem):
-    global flag
-    flag = '3'
-    bot.send_message(mensagem.from_user.id,f'''{mensagem.from_user.first_name}, selecione uma lingua:
+    user_id = mensagem.from_user.id
+    bot.send_message(user_id, f'''{mensagem.from_user.first_name}, selecione uma lingua:
 /portuguese: Traduzo para português
 /english: Traduzo para inglês''')
-    
-#/portuguese
+
+# /portuguese
 @bot.message_handler(commands=['portuguese'])
 def portuguese(mensagem):
-    global flag
-    flag = '4'
-    bot.send_message(mensagem.from_user.id,f"Beleza, manda a audio pro PAPI aqui, que eu traduzo para você")
+    user_id = mensagem.from_user.id
+    user_states[user_id] = UserState()
+    user_states[user_id].flag = '4'
+    bot.send_message(user_id, f"Beleza, manda a audio pro PAPI aqui, que eu traduzo para você")
 
-#/english
+# /english
 @bot.message_handler(commands=['english'])
 def english(mensagem):
-    global flag
-    flag = '5'
-    bot.send_message(mensagem.from_user.id,f"Beleza, manda a audio pro PAPI aqui, que eu traduzo para você")
+    user_id = mensagem.from_user.id
+    user_states[user_id] = UserState()
+    user_states[user_id].flag = '5'
+    bot.send_message(user_id, f"Beleza, manda a audio pro PAPI aqui, que eu traduzo para você")
 
+# /separate
 @bot.message_handler(commands=['separate'])
 def separate_elements(mensagem):
-    global flag
-    flag = '6'
-    bot.send_message(mensagem.from_user.id,f"Beleza, me manda o aúdio que o PAPI separa para você")
+    user_id = mensagem.from_user.id
+    user_states[user_id] = UserState()
+    user_states[user_id].flag = '6'
+    bot.send_message(user_id, f"Beleza, me manda o áudio que o PAPI separa para você")
 
-@bot.message_handler(content_types=['voice','audio'])
+# /reset
+@bot.message_handler(commands=['reset'])
+def reset_state(mensagem):
+    user_id = mensagem.from_user.id
+    user_states[user_id] = UserState()
+    bot.send_message(user_id, "Estado redefinido.")
+
+@bot.message_handler(content_types=['voice', 'audio'])
 def audio(mensagem):
-    global flag
-    #Resumo
-    if flag=='1':
+    user_id = mensagem.from_user.id
+    current_user_state = user_states.get(user_id, UserState())
+
+    if current_user_state.flag == '1':
+        # Resumo
         file_name = process_audio(mensagem)
-        resumo(file_name,mensagem)
-        #Função do resumo aqui
-        flag = '0'
-    #Totext
-    elif flag=='2':
+        resumo(file_name,portuguese,2, mensagem) # LEMBRAR QUE DEPOIS PODE PERGUNTAR OS PARAMETROS DE LANGUAGE E NÚMERO DE FRASES
+        current_user_state.flag = '0'
+    elif current_user_state.flag == '2':
+        # Totext
         file_name = process_audio(mensagem)
-        transcription(file_name,mensagem)
-        #Função do totext aqui
-        flag = '0'
-    #Translate
-    elif flag=='4':
+        transcription(file_name, mensagem)
+        current_user_state.flag = '0'
+    elif current_user_state.flag == '4':
+        # Translate
         file_name = process_audio(mensagem)
-        translate_audio(file_name,mensagem,"Transcription-TTS")
-        flag = '0'    
-    elif flag=='5':
+        translate_audio(file_name, mensagem, "Transcription-TTS")
+    elif current_user_state.flag == '5':
+        # Translate (english)
         file_name = process_audio(mensagem)
-        translate_audio(file_name,mensagem,"Transcription-TTS-(To-English)")
-        #Função do translate(english) aqui
-    elif flag=='6':
+        translate_audio(file_name, mensagem, "Transcription-TTS-(To-English)")
+    elif current_user_state.flag == '6':
+        # Separate
         file_name = process_audio(mensagem)
-        stem_separation(file_name,mensagem)
-        #Conection.stem_separation(file_name,"Job_name",'Stem_Separation','Vocal')
-        flag = '0'
+        stem_separation(file_name, mensagem)
+
+    # Reinicializa o estado do usuário após a conclusão da ação
+    user_states[user_id] = UserState()
+
 
 bot.polling()#Gera o loop infinito da conversa do chat
-
-
